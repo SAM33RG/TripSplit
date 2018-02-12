9053164c1b21e688afdfe8d6b726cda2ff099658
SAM33RG <sameergangar1@gmail.com>
2018-02-13 01:44:29 +0530

    added function to check if user is logined to login activity

--- app/src/main/java/me/sam33r/tripsplit/LoginActivity.java
+++ app/src/main/java/me/sam33r/tripsplit/LoginActivity.java
@@ -2,12 +2,40 @@ package me.sam33r.tripsplit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button login;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.email_ET);
+        password = (EditText) findViewById(R.id.password_ET);
+        login = (Button) findViewById(R.id.login_bt);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+
 }
